49dc71a93f105edbad3c53a5c89aba0f1b2d2971
SychAndrii <asych@myseneca.ca>
2024-09-14 11:26:46 -0400

    Added dependencies to integrate, langchain, openai, and pinecone

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 import os
+import time
 from pinecone import ServerlessSpec
+from langchain.vectorstores import Pinecone as LangChainPinecone
 from pinecone.grpc import PineconeGRPC as Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
 
-pc = Pinecone(api_key=os.getenv("pinecone_api_key"))
-
-index_name = "example-index"
-index = pc.Index(index_name)
-
-
+openai_api_key = os.environ.get("openai_api_key")
+pinecone_cloud = os.environ.get("pinecone_cloud")
+pinecone_region = os.environ.get("pinecone_region")
